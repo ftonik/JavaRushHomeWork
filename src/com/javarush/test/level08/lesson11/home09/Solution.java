4de6d0428dd9a7775e16d23f417f252fcb6c47ba
ftonik <ftonik@gmail.com>
2015-07-31 09:55:25 +0300

    well

--- src/com/javarush/test/level08/lesson11/home09/Solution.java
+++ src/com/javarush/test/level08/lesson11/home09/Solution.java
@@ -1,26 +1,23 @@
 package com.javarush.test.level08.lesson11.home09;
 
-/* Работа с датой
-1. Реализовать метод isDateOdd(String date) так, чтобы он возвращал true, если количество дней с начала года - нечетное число, иначе false
-2. String date передается в формате MAY 1 2013
-Пример:
-JANUARY 1 2000 = true
-JANUARY 2 2020 = false
-*/
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) throws ParseException {
-        System.out.println(isDateOdd("02.03.1991"));
+        String date = "JANUARY 2 2020";
+        System.out.println(date + " = " + isDateOdd(date));
     }
 
-    public static boolean isDateOdd(String date) throws ParseException {
-        SimpleDateFormat format = new SimpleDateFormat();
-        format.applyPattern("dd.MM.yyyy");
-        Date docDate= format.parse(date);
-        return true;
+    public static boolean isDateOdd(String date) {
+        Date curDate = new Date(date);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("D");
+        String dayOldFormat = dateFormat.format(curDate);
+       // System.out.println(dayOldFormat);
+        int dayNewFormat = Integer.parseInt(dayOldFormat);
+        //System.out.println(dayNewFormat % 2 != 0);
+        return dayNewFormat % 2 != 0;
     }
 }
+
